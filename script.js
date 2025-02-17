d9ccf3fbff9df523e985eb2ffc4b229ceb548392
OsazuwaEmmanuel <osazuwaemmanuel25@gmail.com>
2025-02-17 18:59:35 +0100

    Clear all empty fields

--- script.js
+++ script.js
@@ -10,25 +10,27 @@ const fileContent = fs.readFileSync(filePath, 'utf8');
 
 let newBody = fileContent.split('\n');
 let newBody1 = "";
-let clear_sh = "";
 
 for(let i=0; i<newBody.length; ++i){
     if(newBody[i].includes("### Screenshots") && i+5<newBody.length){
-        //if(newBody[i+2].includes("```bash") && newBody[i+4].includes("```")){
-            //clear_sh = ;
-            if(newBody.slice(i, i+6).join('').replace(/\s+/g, '') === "###Screenshots```bash```"){
-                newBody.splice(i, 6, ...Array(6).fill(""));
-            }
-            /*newBody[i] = "_-1";
-            newBody[i+1] = "_-1";
-            newBody[i+2] = "_-1";
-            newBody[i+3] = "_-1";
-            newBody[i+4] = "_-1";*/
-            //newBody.splice(i, 5, ...Array(5).fill("_-1"));
-        //}
+        if(newBody.slice(i, i+6).join('').replace(/\s+/g, '') === "###Screenshots```bash```"){
+            newBody.splice(i, 6, ...Array(6).fill(""));
+        }
     }
-    if(newBody[i].includes("_No response_")){
-        newBody[i] = "";
+    if(newBody[i].includes("### Logs") && i+5<newBody.length){
+        if(newBody.slice(i, i+6).join('').replace(/\s+/g, '') === "###Logs```bash```"){
+            newBody.splice(i, 6, ...Array(6).fill(""));
+        }
+    }
+    if(newBody[i].includes("### Browsers") && i+2<newBody.length){
+        if(newBody.slice(i, i+3).join('').replace(/\s+/g, '') === "###Browsers"){
+            newBody.splice(i, 3, ...Array(3).fill(""));
+        }
+    }
+    if(newBody[i].includes("### OS") && i+2<newBody.length){
+        if(newBody.slice(i, i+3).join('').replace(/\s+/g, '') === "###OS"){
+            newBody.splice(i, 3, ...Array(3).fill(""));
+        }
     }
     if(newBody[i] != ""){
         newBody1 += newBody[i] + '\n';
@@ -38,5 +40,3 @@ console.log("file content: ");
 console.log(fileContent.split('\n'));
 console.log("new body: ");
 console.log(newBody1.split('\n'));
-console.log("clear sh: ");
-console.log(clear_sh);
