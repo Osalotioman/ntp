980faea77bab51f9cde701cbe98f64516a67ebbe
OsazuwaEmmanuel <osazuwaemmanuel25@gmail.com>
2025-02-17 18:49:19 +0100

    Complete removal of unused screenshots section complete.

--- script.js
+++ script.js
@@ -10,21 +10,27 @@ const fileContent = fs.readFileSync(filePath, 'utf8');
 
 let newBody = fileContent.split('\n');
 let newBody1 = "";
+let clear_sh = "";
 
 for(let i=0; i<newBody.length; ++i){
-    if(newBody[i].includes("### Screenshots") && i+4<newBody.length){
-        if(newBody[i+2].includes("```bash") && newBody[i+4].includes("```")){
-            newBody[i] = "_-1";
+    if(newBody[i].includes("### Screenshots") && i+5<newBody.length){
+        //if(newBody[i+2].includes("```bash") && newBody[i+4].includes("```")){
+            //clear_sh = ;
+            if(newBody.slice(i, i+6).join('').replace(/\s+/g, '') === "###Screenshots```bash```"){
+                newBody.splice(i, 6, ...Array(6).fill(""));
+            }
+            /*newBody[i] = "_-1";
             newBody[i+1] = "_-1";
             newBody[i+2] = "_-1";
             newBody[i+3] = "_-1";
-            newBody[i+4] = "_-1";
-        }
+            newBody[i+4] = "_-1";*/
+            //newBody.splice(i, 5, ...Array(5).fill("_-1"));
+        //}
     }
     if(newBody[i].includes("_No response_")){
-        newBody[i] = "_-1";
+        newBody[i] = "";
     }
-    if(newBody[i] != "_-1"){
+    if(newBody[i] != ""){
         newBody1 += newBody[i] + '\n';
     }
 }
@@ -32,3 +38,5 @@ console.log("file content: ");
 console.log(fileContent.split('\n'));
 console.log("new body: ");
 console.log(newBody1.split('\n'));
+console.log("clear sh: ");
+console.log(clear_sh);
